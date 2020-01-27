61ba0aa486fd6d5d0bc14cf2f4b03c6f4726d97d
Mirlan Ipasov <mipasov@gmail.com>
2020-01-27 15:29:54 +0600

    change defer to sync

--- modules/core/src/main/scala/relay/http/routes/admin/AdminBrandRoutes.scala
+++ modules/core/src/main/scala/relay/http/routes/admin/AdminBrandRoutes.scala
@@ -1,33 +1,32 @@
-
 package relay.http.routes.admin
 
-import cats.Defer
-import org.http4s.circe.JsonDecoder
-import relay.effects.`package`.MonadThrow
-import relay.algebras.Brands
+import cats.effect.Sync
+import io.circe.refined._
+import org.http4s._
 import org.http4s.dsl.Http4sDsl
-import org.http4s.AuthedRoutes
+import org.http4s.server._
+import relay.algebras.Brands
+import relay.domain.brand._
 import relay.domain.auth.AdminUser
-import relay.domain.brand.BrandParam
-import org.http4s.server.`package`.AuthMiddleware
-import org.http4s.HttpRoutes
-import org.http4s.server.Router
+import relay.http.decoder._
+import relay.http.json._
 
-final class AdminBrandRoutes[F[_]: Defer: JsonDecoder: MonadThrow](
+final class AdminBrandRoutes[F[_]: Sync](
     brands: Brands[F]
 ) extends Http4sDsl[F] {
 
-    private[admin] val prefixPath = "/brands"
+  private[admin] val prefixPath = "/brands"
 
-    private val httpRoutes: AuthedRoutes[AdminUser, F] = 
-        AuthedRoutes.of {
-            case ar @ POST -> Root as _ => 
-                ar.req.decodeR[BrandParam] { bp => 
-                    Created(brands.create(bp.toDomain))
-                }
+  private val httpRoutes: AuthedRoutes[AdminUser, F] =
+    AuthedRoutes.of {
+      case ar @ POST -> Root as _ =>
+        ar.req.decodeR[BrandParam] { bp =>
+          Created(brands.create(bp.toDomain))
         }
+    }
 
-    def routes(authMiddleware: AuthMiddleware[F, AdminUser]): HttpRoutes[F] = Router(
-        prefixPath -> authMiddleware(httpRoutes)
-    )
+  def routes(authMiddleware: AuthMiddleware[F, AdminUser]): HttpRoutes[F] = Router(
+    prefixPath -> authMiddleware(httpRoutes)
+  )
+
 }
